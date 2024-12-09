8e0f47d5549474003e14e5539c5f0e0e8d02eb9f
Pepel <lera.kalashnikova2018@gmail.com>
2024-12-09 15:08:52 +0200

    added task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [currentTask, setCurrentTask] = useState('task1');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <nav className="task-list">
+        <button onClick={() => setCurrentTask('task1')}>Task 1</button>
+        <button onClick={() => setCurrentTask('task2')}>Task 2</button>
+        <button onClick={() => setCurrentTask('task3')}>Task 3</button>
+      </nav>
 
-export default App
+      <div>
+        {currentTask === 'task1' && (
+          <Profile
+            name={userData.username}
+            tag={userData.tag}
+            location={userData.location}
+            image={userData.avatar}
+            stats={userData.stats}
+          />
+        )}
+        {currentTask === 'task2' && <FriendList friends={friends} />}
+        {currentTask === 'task3' && <TransactionHistory items={transactions} />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
